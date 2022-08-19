4bdb7bee40541998ea8a827ce8e90a15de351765
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-08-19 19:57:50 +0100

    :lock: security fix

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const productRouter = require('./routes/productRoutes');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/products', productRouter);
+
 module.exports = app;
---
+++ controllers/productController.js
@@ -0,0 +1,3 @@
+const Product = require('../models/productModel');
+
+exports.getProducts = (req, res, next) => {};
